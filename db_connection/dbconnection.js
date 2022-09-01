d5dcf8a1ecbbc746541a44078c6bba1b955d29d2
prasannakarki77 <prasanna.karki77@gmail.com>
2022-09-01 19:35:54 +0545

    replace db with local connection

--- db_connection/dbconnection.js
+++ db_connection/dbconnection.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.connect(
-  "mongodb+srv://kakashi77:prk.karki123@cluster0.4fn5zfq.mongodb.net/nrental_db?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect("mongodb://127.0.0.1:27017/n-rental_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
